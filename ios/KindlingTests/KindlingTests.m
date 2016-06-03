d29c13177e305c8158da4513b356e7ac8529f6e3
Lauri Lavanti <lauri.lavanti@futurice.com>
2016-06-03 08:53:45 +0300

    Update react-native to 0.26 and cocoapods to 1.0

--- ios/KindlingTests/KindlingTests.m
+++ ios/KindlingTests/KindlingTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 240
 #define TEXT_TO_LOOK_FOR @"I'm bored!"
 
-@interface KindlingTests : XCTestCase
+@interface Golf-scorebookTests : XCTestCase
 
 @end
 
-@implementation KindlingTests
+@implementation Golf-scorebookTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
